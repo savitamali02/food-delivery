8c681633c67c486cca5b92092198070c304109b1
savitamali02 <savitam247@gmail.com>
2025-04-07 15:28:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home/Home";
+import About  from "./pages/Home/About";
+import Menu from "./pages/Home/Menu";
+import Shop from "./pages/Home/Shop";
+import Blog from "./pages/Home/Blog";
+import Contact from "./pages/Home/Contact";
+//import Header from "../components/layout/Header";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        
       </Routes>
 
     </Router>
---
+++ src/pages/Home/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className="p-4">
+      <h1>About Us</h1>
+      <p>This is the About page. You navigated here from the navbar.</p>
+    </div>
+  )
+}
+
+export default About;
