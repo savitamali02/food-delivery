4c18dc21f54d6fd065b366640a2f6c9174882045
savitamali02 <savitam247@gmail.com>
2025-04-15 16:41:49 +0530

    final commit

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -22,38 +22,38 @@ function Header() {
 
   return (
     <header>
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        className={`${nav ? "sticky" : ""}`}
-      >
+      <Navbar collapseOnSelect expand="lg" className={`${nav ? "sticky" : ""}`}>
         <Container>
           <Navbar.Brand as={Link} to="/" className="logo">
             <img src={Logo} alt="Logo" className="img-fluid" />
           </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
+          {/* Icons Container */}
+          <div className="d-flex align-items-center order-lg-3">
+            <Nav.Link as={Link} to="/wishlist" className="px-2">
+              <div className="wishlist">
+                <i className="bi bi-heart fs-5"></i>
+                <em className="roundpoint">{wishlistItems.length}</em>
+              </div>
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/cart" className="px-2">
+              <div className="cart">
+                <i className="bi bi-bag fs-5"></i>
+                <em className="roundpoint">{cartItems.length}</em>
+              </div>
+            </Nav.Link>
+
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          </div>
+
+          <Navbar.Collapse id="responsive-navbar-nav" className="order-lg-2">
             <Nav className="ms-auto">
               <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/about">About</Nav.Link>
               <Nav.Link as={Link} to="/menu">Our Menu</Nav.Link>
               <Nav.Link as={Link} to="/blog">Review</Nav.Link>
               <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-
-              <Nav.Link as={Link} to="/wishlist">
-                <div className="wishlist">
-                  <i className="bi bi-heart fs-5"></i>
-                  <em className="roundpoint">{wishlistItems.length}</em>
-                </div>
-              </Nav.Link>
-
-              <Nav.Link as={Link} to="/cart">
-                <div className="cart">
-                  <i className="bi bi-bag fs-5"></i>
-                  <em className="roundpoint">{cartItems.length}</em>
-                </div>
-              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
